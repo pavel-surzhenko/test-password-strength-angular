343a01f4ffa3738ed44b831a04d07c469145530e
Pavlo Surzhenko <pavel.surzhenko@icloud.com>
2023-02-21 23:14:33 +0200

    add some style

--- src/app/password-strength-checker/password-strength-checker.component.ts
+++ src/app/password-strength-checker/password-strength-checker.component.ts
@@ -7,27 +7,27 @@ import { Component } from '@angular/core';
 })
 export class PasswordStrengthCheckerComponent {
   password = '';
-  emptyColor = 'gray';
   section1Color = 'gray';
   section2Color = 'gray';
+  section3Color = 'gray';
 
   onPasswordChange() {
     if (this.password.length > 0 && this.password.length < 8) {
-      this.emptyColor = 'red';
       this.section1Color = 'red';
       this.section2Color = 'red';
+      this.section3Color = 'red';
     } else if (/^\d+$|^[a-z]+$|^[\W_]+$/i.test(this.password)) {
-      this.emptyColor = 'red';
-      this.section1Color = 'gray';
+      this.section1Color = 'red';
       this.section2Color = 'gray';
+      this.section3Color = 'gray';
     } else if (/^(?=.*[a-z])(?=.*\d)[a-z\d]+$/i.test(this.password) || /^(?=.*[a-z])(?=.*[\W_])[^\d]+$/i.test(this.password) || /^(?=.*[0-9])(?=.*[\W_])[\d\W_]+$/i.test(this.password)) {
-      this.emptyColor = 'yellow';
       this.section1Color = 'yellow';
-      this.section2Color = 'gray';
+      this.section2Color = 'yellow';
+      this.section3Color = 'gray';
     } else if (/(?=.*\d)(?=.*[a-z])(?=.*[\W_]).+/i.test(this.password)) {
-      this.emptyColor = 'green';
       this.section1Color = 'green';
       this.section2Color = 'green';
+      this.section3Color = 'green';
     }
   }
 }

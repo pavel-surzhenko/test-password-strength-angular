7f5937ec5dcbd6471787bb14fd8e11437a9683fa
Pavlo Surzhenko <pavel.surzhenko@icloud.com>
2023-02-21 23:36:38 +0200

    add check functions

--- src/app/password-strength-checker/password-strength-checker.component.ts
+++ src/app/password-strength-checker/password-strength-checker.component.ts
@@ -5,29 +5,40 @@ import { Component } from '@angular/core';
   templateUrl: './password-strength-checker.component.html',
   styleUrls: ['./password-strength-checker.component.scss']
 })
+
 export class PasswordStrengthCheckerComponent {
   password = '';
-  section1Color = 'gray';
-  section2Color = 'gray';
-  section3Color = 'gray';
+  easySectionColor = 'gray';
+  mediumSectionColor = 'gray';
+  strongSectionColor = 'gray';
+
+  isEasy(password: string) {
+    return /^\d+$|^[a-z]+$|^[\W_]+$/i.test(password)
+  }
+
+  isMedium(password: string) {
+    const noSymbol = /^(?=.*[a-z])(?=.*\d)[a-z\d]+$/i.test(password);
+    const noDigits = /^(?=.*[a-z])(?=.*[\W_])[^\d]+$/i.test(password)
+    const noLetters = /^(?=.*[0-9])(?=.*[\W_])[\d\W_]+$/i.test(password)
+
+    return noSymbol || noDigits || noLetters
+  }
+
+  isStrong(password: string) {
+    return /(?=.*\d)(?=.*[a-z])(?=.*[\W_]).+/i.test(password)
+  }
 
   onPasswordChange() {
     if (this.password.length > 0 && this.password.length < 8) {
-      this.section1Color = 'red';
-      this.section2Color = 'red';
-      this.section3Color = 'red';
-    } else if (/^\d+$|^[a-z]+$|^[\W_]+$/i.test(this.password)) {
-      this.section1Color = 'red';
-      this.section2Color = 'gray';
-      this.section3Color = 'gray';
-    } else if (/^(?=.*[a-z])(?=.*\d)[a-z\d]+$/i.test(this.password) || /^(?=.*[a-z])(?=.*[\W_])[^\d]+$/i.test(this.password) || /^(?=.*[0-9])(?=.*[\W_])[\d\W_]+$/i.test(this.password)) {
-      this.section1Color = 'yellow';
-      this.section2Color = 'yellow';
-      this.section3Color = 'gray';
-    } else if (/(?=.*\d)(?=.*[a-z])(?=.*[\W_]).+/i.test(this.password)) {
-      this.section1Color = 'green';
-      this.section2Color = 'green';
-      this.section3Color = 'green';
+      this.easySectionColor = this.mediumSectionColor = this.strongSectionColor = 'red';
+    } else if (this.isEasy(this.password)) {
+      this.easySectionColor = 'red';
+      this.mediumSectionColor = this.strongSectionColor = 'gray';
+    } else if (this.isMedium(this.password)) {
+      this.easySectionColor = this.mediumSectionColor = 'yellow';
+      this.strongSectionColor = 'gray';
+    } else if (this.isStrong(this.password)) {
+      this.easySectionColor = this.mediumSectionColor = this.strongSectionColor = 'green';
     }
   }
 }
